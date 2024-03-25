6dc74fac847dabaf76aa07b934fac87c22d619f1
kaviyarasu kasi <kaviyarasu@mulecraft.in>
2024-03-25 13:00:12 +0530

    fixing bug

---
+++ src/pages/Access-Management/AM-businessGroup.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Nav from "../../components/NavbarHome";
+import Sidebar from "../../components/sidebar";
+import sections from "./utils/AM-sidebar";
+
+
+export default function AMBusinessGroup({ name, pathValue }) {
+    const [activeItem, setActiveItem] = useState('BusinessGroups');
+
+
+    const handleItemSelect = (itemName) => {
+        setActiveItem(itemName);
+    };
+    return (
+        <>
+            <div className="home">
+
+                <Nav name={name} pathValue={pathValue} />
+                <div className="Wrapper">
+                    <Sidebar
+                        sections={sections}
+                        activeItem={activeItem}
+                        onItemSelect={handleItemSelect}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/pages/Access-Management/AM-pending.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Nav from "../../components/NavbarHome";
+import Sidebar from "../../components/sidebar";
+import sections from "./utils/AM-sidebar";
+import { Box, Flex } from "@chakra-ui/react";
+import HorizontalSidebar from "../../components/HorizontalSidebar";
+import userTab from "./utils/AM-userTab";
+import PendingInvitation from "../../components/AM-Component/pendingInvitation";
+
+
+export default function AMPending({ name }) {
+    const [activeItem, setActiveItem] = useState('PendingInvitations');
+
+
+    const handleItemSelect = (itemName) => {
+        setActiveItem(itemName);
+    };
+    return (
+        <>
+            <div className="home">
+                <Nav name={name} />
+                <div className="Wrapper">
+                    <Flex>
+                        <Box>
+                            <Sidebar
+                                sections={sections}
+                                activeItem={activeItem}
+                                onItemSelect={handleItemSelect}
+                            />
+                        </Box>
+                        <Flex direction="column" w="full" ml="200" mt="200">
+                            <HorizontalSidebar
+                                sections={userTab}
+                                activeItem={activeItem}
+                                onItemSelect={handleItemSelect}
+                            />
+                            <Box p="4" w="100%" alignItems="center" justifyContent="center" mt="-60px">
+                                <PendingInvitation />
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </div>
+            </div >
+        </>
+    );
+}
