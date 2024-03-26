ca8896073821721eafa423072d97d0011811801b
Kesavarajan10 <120188691+Kesavarajan10@users.noreply.github.com>
2024-03-26 11:52:42 +0530

    issues

--- src/components/HeaderLogo.jsx
+++
@@ -1,9 +0,0 @@
-import CompanyLogo from "/Images/mulelogo.svg";
-import "../assets/Common.css";
-export default function HeaderLogo() {
-  return (
-    <>
-      <img src={CompanyLogo} alt="company logo" className="header-comp" />
-    </>
-  );
-}
