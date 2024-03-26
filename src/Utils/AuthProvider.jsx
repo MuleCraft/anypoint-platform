552a77e92f0e238f8d5bb59d0ee6c9154224d86c
Kesavarajan10 <120188691+Kesavarajan10@users.noreply.github.com>
2024-03-26 17:47:09 +0530

    Squashed commit of the following:
    commit ce81b5f23c03e2a9d8ea3041ef130d7bb1e5e78b
    Author: Kesavarajan10 <120188691+Kesavarajan10@users.noreply.github.com>
    Date:   Tue Mar 26 17:41:41 2024 +0530
    
        Reload issue
    
        Reload issue and session persistance is achieved
    
    commit 0d27ed17b86967ab151fb6b1d27a010b3b77ce4b
    Author: Kesavarajan10 <120188691+Kesavarajan10@users.noreply.github.com>
    Date:   Mon Mar 25 17:42:34 2024 +0530
    
        Squashed commit of the following:
    
        commit 64cb333a4a9c78f096c29a21ce43028603de1975
        Merge: e12d6e0 710a9d1
        Author: ShanRP <152154104+ShanRP@users.noreply.github.com>
        Date:   Mon Mar 25 17:22:47 2024 +0530
    
            Merge pull request #38 from MuleCraft/feature/AM-business-group
    
            Business Group form commit
    
        commit 710a9d18702c0a669e5a6e2b6d03e126a43c7f23
        Author: shanmathy <shanmathy@mulecraft.in>
        Date:   Mon Mar 25 17:21:56 2024 +0530
    
            Business Group form commit

--- src/Utils/AuthProvider.jsx
+++ src/Utils/AuthProvider.jsx
@@ -4,36 +4,53 @@ import supabase from "./supabase";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [session, setSession] = useState(null);
-  const [subscription, setSubscription] = useState(null);
+  const [session, setSession] = useState(() => {
+    const storedSession = localStorage.getItem("session");
+    return storedSession ? JSON.parse(storedSession) : null;
+  });
 
   useEffect(() => {
     const handleAuthStateChange = (_event, session) => {
       setSession(session);
+      localStorage.setItem("session", JSON.stringify(session));
     };
 
     const authSubscription = supabase.auth.onAuthStateChange(
       handleAuthStateChange
     );
 
+    // Retrieve initial session
     const initialSession = supabase.auth.getSession();
     if (initialSession) {
       setSession(initialSession);
+      localStorage.setItem("session", JSON.stringify(initialSession));
     }
 
-    setSubscription(authSubscription);
-
     return () => {
-      if (subscription) {
-        subscription.unsubscribe();
+      if (
+        authSubscription &&
+        typeof authSubscription.unsubscribe === "function"
+      ) {
+        authSubscription.unsubscribe();
       }
     };
   }, []);
 
+  const refreshSession = async () => {
+    try {
+      const refreshedSession = await supabase.auth.refreshSession();
+      setSession(refreshedSession);
+      localStorage.setItem("session", JSON.stringify(refreshedSession));
+    } catch (error) {
+      console.error("Error refreshing session:", error.message);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ session, setSession }}>
+    <AuthContext.Provider value={{ session, setSession, refreshSession }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
 export { AuthProvider, AuthContext };
