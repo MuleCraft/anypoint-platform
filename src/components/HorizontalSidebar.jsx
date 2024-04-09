c897620447bd56019b9f1ef20e27e9f21cf5084c
kaviyarasu kasi <kaviyarasu@mulecraft.in>
2024-04-09 16:09:27 +0530

    fix zindex

--- src/components/HorizontalSidebar.jsx
+++ src/components/HorizontalSidebar.jsx
@@ -13,7 +13,7 @@ const HorizontalSidebar = ({ sections, activeItem, onItemSelect }) => {
             h="auto"
             position="fixed"
             top="20"
-            zIndex={99}
+            zIndex={0}
         >
             {sections.map((section, sectionIndex) => (
                 <Box key={sectionIndex} mr={1}>
