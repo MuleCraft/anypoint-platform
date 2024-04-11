64c5ef7eb733e7bc641d7d0914d1a8a7d5aac980
Kesavarajan10 <120188691+Kesavarajan10@users.noreply.github.com>
2024-04-11 13:25:13 +0530

    fix

--- src/Utils/BgTableRows.js
+++ src/Utils/BgTableRows.js
@@ -1,19 +1,18 @@
 import supabase from "./supabase";
 
-export default async function fetchBgTableRows(currentUserName){
-    // console.log('Fetch rows fn invoked!');
-    const { data, error } = await supabase
-        .schema('mc_cap_develop')
-        .from('businessGroup')
-        .select("*")
-        .eq('userName',currentUserName)
+export default async function fetchBgTableRows(currentUserName) {
+  // console.log('Fetch rows fn invoked!');
+  const { data, error } = await supabase
+    .schema("mc_cap_develop")
+    .from("businessgroup")
+    .select("*")
+    .eq("userName", currentUserName);
 
-    if (data) {
-        // console.log('Fetch Bg table rows.', data);
-        return data;
-    }
-    else {
-        console.log(error);
-        return error;
-    }
+  if (data) {
+    // console.log('Fetch Bg table rows.', data);
+    return data;
+  } else {
+    console.log(error);
+    return error;
+  }
 }
