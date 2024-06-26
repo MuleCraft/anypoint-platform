184109eef85e6818c5a95219e98d7e4ee5f33569
kaviyarasu kasi <95094074+kaviyarasumaran@users.noreply.github.com>
2024-06-26 13:02:24 +0530

    adding router
    add the router based on user in permission, memberships, limites

---
+++ src/pages/Access-Management/Users/AM-Limits.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Nav from "../../../components/NavbarHome";
+import Sidebar from "../../../components/sidebar";
+import sections from "../utils/AM-sidebar";
+
+import { Box, Flex } from "@chakra-ui/react";
+import UserLimits from "../../../components/AM-Component/AM-Users/userLimits";
+
+
+
+export default function AMUsersLimits({ name, pathValue }) {
+    const [activeItem, setActiveItem] = useState('users');
+
+
+    const handleItemSelect = (itemName) => {
+        setActiveItem(itemName);
+    };
+    return (
+        <>
+            <div className="home">
+                <Nav name={name} pathValue={pathValue} />
+                <div className="Wrapper">
+                    <Flex>
+                        <Box>
+                            <Sidebar
+                                sections={sections}
+                                activeItem={activeItem}
+                                onItemSelect={handleItemSelect}
+                            />
+                        </Box>
+                        <Flex direction="column" w="full" ml="180" mt="200">
+
+
+                            <Box p="4" w="100%" alignItems="center" justifyContent="center" mt="-60px">
+                                <UserLimits />
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/pages/Access-Management/Users/AM-membership.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Nav from "../../../components/NavbarHome";
+import Sidebar from "../../../components/sidebar";
+import sections from "../utils/AM-sidebar";
+import { Box, Flex } from "@chakra-ui/react";
+import UserMembership from "../../../components/AM-Component/AM-Users/userMembership";
+
+
+
+export default function AMUsersMembership({ name, pathValue }) {
+    const [activeItem, setActiveItem] = useState('users');
+
+
+    const handleItemSelect = (itemName) => {
+        setActiveItem(itemName);
+    };
+    return (
+        <>
+            <div className="home">
+                <Nav name={name} pathValue={pathValue} />
+                <div className="Wrapper">
+                    <Flex>
+                        <Box>
+                            <Sidebar
+                                sections={sections}
+                                activeItem={activeItem}
+                                onItemSelect={handleItemSelect}
+                            />
+                        </Box>
+                        <Flex direction="column" w="full" ml="180" mt="200">
+
+
+                            <Box p="4" w="100%" alignItems="center" justifyContent="center" mt="-60px">
+                                <UserMembership />
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </div>
+            </div>
+        </>
+    );
+}
